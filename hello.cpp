8b79316c989f4c6b42269343ad12a01ff3cd4475
right_hand <1090062914@qq.com>
2021-03-06 01:10:04 +0800

    为项目添加了CMakeLists.txt使其能够正常进行编译调试

--- hello.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by m1814 on 2021/3/5.
-//
-
-#include "hello.h"
---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by legion on 2021/3/6 0006.
+//
+
+#include <iostream>
+
+int main(){
+    printf("2333");
+}
