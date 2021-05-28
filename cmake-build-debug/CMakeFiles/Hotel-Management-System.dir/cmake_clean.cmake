806052a83189b73335bfc0103c27e0fc8590e2d8
Ashen-twos <15360421037@163.com>
2021-05-28 10:45:18 +0800

    order

---
+++ cmake-build-debug/CMakeFiles/Hotel-Management-System.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hotel-Management-System.dir/Dish/DishInfo.cpp.obj"
+  "CMakeFiles/Hotel-Management-System.dir/Dish/Dishes.cpp.obj"
+  "CMakeFiles/Hotel-Management-System.dir/Kitchen/Kitchen.cpp.obj"
+  "CMakeFiles/Hotel-Management-System.dir/Order/Order.cpp.obj"
+  "CMakeFiles/Hotel-Management-System.dir/Order/OrderInfo.cpp.obj"
+  "CMakeFiles/Hotel-Management-System.dir/main.cpp.obj"
+  "Hotel-Management-System.exe"
+  "Hotel-Management-System.exe.manifest"
+  "Hotel-Management-System.pdb"
+  "libHotel-Management-System.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hotel-Management-System.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
