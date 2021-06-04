139b1865c07bef97ff4d504bbde2475573a30ef0
Ashen-twos <15360421037@163.com>
2021-06-04 23:28:59 +0800

    增加了Storage中增加和消耗多种食材的功能

--- src/Storage/Storage.cpp
+++ src/Storage/Storage.cpp
@@ -5,32 +5,35 @@
 
 #include "Storage.h"
 
+map<string,int> Storage::MaterialsList;
+
+//获取本地食材列表
 void Storage::GetMaterialsList()
 {
     int n,num;
     string str;
     ifstream data;
     data.open("..\\lib\\Storage.txt");
-    data >> n;
+    data >> n;                  //食材种类
     for(int i=1; i<=n; i++)
     {
-        data >> str >> num;
+        data >> str >> num;     //读入名称、数量
         MaterialsList.insert({str,num});
     }
-    data.close();
+    data.close();               //关输入流
 }
-
+//保存食材列表到本地
 void Storage::SaveMaterialsList()
 {
     ofstream data;
     data.open("..\\lib\\Storage.txt");
-    data << MaterialsList.size() << '\n';
-    for(auto x : MaterialsList)
-        data << x.first << ' ' << x.second << '\n';
-    data.close();
+    data << MaterialsList.size() << '\n';               //食材种类
+    for(const auto& x : MaterialsList)
+        data << x.first << ' ' << x.second << '\n';     //写入名称、数量
+    data.close();                                       //关输出流
 }
-
-bool Storage::AddMaterial(string name, int num)
+//添加一种食材
+bool Storage::AddMaterial(const string& name, int num)
 {
     if(MaterialsList.count(name))
         MaterialsList[name] += num;
@@ -39,8 +42,21 @@ bool Storage::AddMaterial(string name, int num)
     SaveMaterialsList();
     return true;
 }
-
-bool Storage::TakeMaterial(string name, int num)
+//添加多种食材
+bool Storage::AddMaterial(const map<string, int>& Materials)
+{
+    for(const auto& x:Materials)
+    {
+        if(MaterialsList.count(x.first))
+            MaterialsList[x.first] += x.second;
+        else
+            MaterialsList[x.first] = x.second;
+    }
+    SaveMaterialsList();
+    return true;
+}
+//消耗一种食材
+bool Storage::TakeMaterial(const string& name, int num)
 {
     if(MaterialsList.count(name)&&MaterialsList[name]>=num)
     {
@@ -53,8 +69,25 @@ bool Storage::TakeMaterial(string name, int num)
     }
     return false;
 }
-
-int Storage::GetMaterialsNum(string name)
+//消耗多种食材
+bool Storage::TakeMaterial(const map<string, int>& Materials)
+{   //保证所有食材均充足
+    for(const auto& x:Materials)
+        if(!MaterialsList.count(x.first)||MaterialsList[x.first]<x.second)
+            return false;
+    //消耗食材
+    for(const auto& x:Materials)
+    {
+        if(x.second==MaterialsList[x.first])
+            MaterialsList.erase(x.first);
+        else
+            MaterialsList[x.first] -= x.second;
+    }
+    Storage::SaveMaterialsList();
+    return true;
+}
+//获取某个食材的数量
+int Storage::GetMaterialsNum(const string& name)
 {
     if(MaterialsList.count(name))
         return MaterialsList[name];
--- src/Storage/Storage.h
+++ src/Storage/Storage.h
@@ -17,16 +17,20 @@ private:
     //食材库存
     static map<string,int> MaterialsList;
 public:
-    //从硬盘中获取食材列表
+    //从本地获取食材列表
     static void GetMaterialsList();
-    //保存当前食材列表到硬盘
+    //保存当前食材列表到本地
     static void SaveMaterialsList();
-    //添加食材
-    static bool AddMaterial(string name, int num);
-    //消耗食材
-    static bool TakeMaterial(string name, int num);
+    //添加一种食材
+    static bool AddMaterial(const string& name, int num);
+    //添加多种食材
+    static bool AddMaterial(const map<string,int>& Materials);
+    //消耗一种食材
+    static bool TakeMaterial(const string& name, int num);
+    //消耗多种食材
+    static bool TakeMaterial(const map<string,int>& Materials);
     //查询食材数量
-    static int GetMaterialsNum(string name);
+    static int GetMaterialsNum(const string& name);
 };
 
 
